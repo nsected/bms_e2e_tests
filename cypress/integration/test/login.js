05833f3b9cf13d5b5aaa78fbb31aa10b6243c377
d.mustaev <d.mustaev@xsolla.com>
2018-08-24 19:33:06 +0500

    1112укпеуЁёё§

--- cypress/integration/test/login.js
+++ cypress/integration/test/login.js
@@ -1,14 +1,13 @@
 context('login', () => {
 it('login', () => {
-    cy.visit('/')
-    cy.get('[name="username"]').type('ttestpublisher@gmail.com').should('have.value', 'ttestpublisher@gmail.com')
-    cy.get('[name="password"]').type('TestTestTest111').should('have.value', 'TestTestTest111')
-    cy.get('[type="submit"]').click()
-    cy.get('.ui-navigation__user').click()
+    cy.visit('/');
+    cy.get('[name="username"]').type('ttestpublisher@gmail.com').should('have.value', 'ttestpublisher@gmail.com');
+    cy.get('[name="password"]').type('TestTestTest111').should('have.value', 'TestTestTest111');
+    cy.get('[type="submit"]').click();
+    cy.get('.ui-navigation__user').click();
     cy.getCookies()
         .then((cookies) => {
-            // cy.log(JSON.stringify(cookies))
-            cy.exec(`echo '${JSON.stringify(cookies)}' >tmp/cookies.json`)
+            cy.writeFile('tmp/cookies.json', JSON.stringify(cookies, null, 4))
         })
 })
 });
