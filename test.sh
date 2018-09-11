8af718db7cdf0b00bb3a759493e7c6442fd15049
d.mustaev <d.mustaev@gmail.com>
2018-09-12 02:11:10 +0500

    soska

--- test.sh
+++ test.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 rm -rf report
-npx cypress run  --spec 'cypress/integration/publisher/store/game-keys/**/*' --record --key 1cda45fb-63b2-4a42-ae9c-6cf3eb6a58aa
-#npx cypress run  --spec 'cypress/integration/service/login.js' --record --key 1cda45fb-63b2-4a42-ae9c-6cf3eb6a58aa
-#npx cypress run  --spec 'cypress/integration/publisher/store/game-keys/**/*' --record --key 1cda45fb-63b2-4a42-ae9c-6cf3eb6a58aa &
-#npx cypress run  --spec 'cypress/integration/publisher/store/subscriptions/**/*' --record --key 1cda45fb-63b2-4a42-ae9c-6cf3eb6a58aa &
+#npx cypress run  --spec 'cypress/integration/publisher/store/game-keys/**/*' --record --key 1cda45fb-63b2-4a42-ae9c-6cf3eb6a58aa
+npx cypress run  --spec 'cypress/integration/service/login.js' --record --key 1cda45fb-63b2-4a42-ae9c-6cf3eb6a58aa
+npx cypress run  --spec 'cypress/integration/publisher/store/game-keys/**/*' --record --key 1cda45fb-63b2-4a42-ae9c-6cf3eb6a58aac-6cf3eb6a58aa &
+npx cypress run  --spec 'cypress/integration/publisher/store/subscriptions/**/*' --record --key 1cda45fb-63b2-4a42-ae9c-6cf3eb6a58aa &
 #wait
 #npx cypress run  --spec 'cypress/integration/publisher/store/virtual-currency/**/*'  --record --key 1cda45fb-63b2-4a42-ae9c-6cf3eb6a58aa &
 #npx cypress run  --spec 'cypress/integration/publisher/store/virtual-items/**/*'  --record --key 1cda45fb-63b2-4a42-ae9c-6cf3eb6a58aa &
 #wait
 
+sleep 2
+node merge-reports.js
+npx marge report/all.json
