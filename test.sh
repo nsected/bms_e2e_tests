586bc68c6710dbf1eedd95d7e25c36e40a08f7ff
d.mustaev <d.mustaev@gmail.com>
2018-09-24 03:25:03 +0500

    api mocks

--- test.sh
+++ test.sh
@@ -2,16 +2,16 @@
 rm -rf report #очищаем папку с отчетом
 rm -rf tmp #очищаем временную папку
 mkdir -p tmp # временная папка
-echo "{}" > tmp/cookies.json #создаем пустой файл cookies
+echo "{}" > tmp/cookies.json
 npx cypress run  --spec 'cypress/integration/service/login.js'
 npx cypress run  --spec 'cypress/integration/publisher/store/subscriptions/**/*' &
 npx cypress run  --spec 'cypress/integration/service/error.js' &
 wait
-sleep 2
-rm report/*.html # очищаем ненужные html отчеты
+rm report/*.html
 node merge-reports.js # мержим json отчеты в один файл. На данный момент используется самописный скрипт для мержа отчетов.
 npx marge report/report.json -o report # генерируем html файл для визуального просмотра отчета
 #мы можем использовать систему отчетов cypress dashboard
 #cypress использует сложную систему синхронизации на основе проприетарного сервиса dashboard для паралельного исполнения тестов,
 # но нам доступно простейшее выполнение процесса cypress параллельно, с разным набором тест-кейсов.
 # на данный момент cypress позволет использовать проприетарную систему отчетов бесплатно
+
